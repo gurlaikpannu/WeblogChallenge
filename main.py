5b43cf3abe6bb835bb2fe40b75829c8f7a169dbd
Gurlaik Pannu <gurlaik.pannu@gmail.com>
2017-08-14 08:44:26 -0400

    Answered: 	1. Count By Sessions (Sessionize) 	2. Average Session Time 	3. Distinct Users by Session

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ path.append("lib/")
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import StringType
-from pyspark.sql.functions import col, split, udf
+from pyspark.sql.functions import col, split, udf, coalesce, lag, lit, sum as pyspark_sum, countDistinct, count
+from pyspark.sql.window import Window
 import schema_builder
 from user_agents import parse as parse_user_agent
+
+
+WINDOW_SIZE = 900
 
 
 def extract_browser_and_version(user_agent):
@@ -33,10 +37,45 @@ data_frame = (
                .load("data/2015_07_22_mktplace_shop_web_log_sample.log.gz")
 )
 
+
+###### DEFINING DISTINCT USER BY CLIENT IP AND "BROWSER NAME & VERSION" ######
 # Strip out port number - Create new colum for client IP without port
 data_frame = data_frame.withColumn("client_ip", split(col("client_ip_port"), ":")[0])
 
 # Extract browser and version from user_agent and create a new column
 data_frame = data_frame.withColumn("browser_ver", browser_ver_udf(col("user_agent")))
+##############################################################################
 
-print(data_frame.show())
+
+###### SESSIONIZE BY CLIENT IP AND WEB BROWSER (WITH VERSION) ######
+window = Window.partitionBy("client_ip", "browser_ver").orderBy("timestamp")
+diff = coalesce(col("timestamp").cast("float") - lag("timestamp", 1).over(window).cast("float"), lit(0))
+indicator = (diff > WINDOW_SIZE).cast("integer")
+session = pyspark_sum(indicator).over(window).alias("session")
+data_frame = data_frame.select("*", session)
+####################################################################
+
+# Count By Sessions
+print(
+  "Count By Sessions are: \n",
+  data_frame.groupBy("session").count().collect()
+)
+
+# Average Session Time
+data_frame = data_frame.withColumn("marginal_session_time", diff)
+print(
+  "Average Session Time is: \n",
+  data_frame.groupBy("session").avg("marginal_session_time").collect()
+)
+
+# Distinct Users by Session
+print(
+  "Distinct Users by Session are: \n",
+  data_frame.groupBy("session").agg(countDistinct("client_ip", "browser_ver")).collect()
+)
+
+# Unique Users With The Longest Session Time
+print(
+  "Unique Users With The Longest Session Time are: \n",
+  data_frame.groupBy("session", "client_ip", "browser_ver").agg({"marginal_session_time": "sum"}).alias("session_time_sums").collect()
+)
